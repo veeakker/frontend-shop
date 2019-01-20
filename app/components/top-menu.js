cc98eb11ef1e24b993a3466f72243514df403cfe
Aad Versteden <madnificent@gmail.com>
2019-01-20 18:13:25 +0100

    Import base web page and make top-menu component
    Base styles and base page have been imported.
    
    We have also constructed the top-menu component.  This component
    handles the top menu.  Including the opening and closing of said
    menu. See:
    - app/templates/components/top-menu.hbs
    - app/components/top-menu.js
    - app/styles/mobile-menu.scss

---
+++ app/components/top-menu.js
@@ -0,0 +1,24 @@
+import Component from '@ember/component';
+import { action, computed } from '@ember-decorators/object';
+
+export default class TopMenuComponent extends Component {
+  classNames = ["top-menu"]
+
+  mobileOpen = false;
+
+  @computed('mobileOpen')
+  get mobileIsOpenClass(){
+    return this.mobileOpen && "mobile-open";
+  }
+
+  @action
+  openNav(){
+    this.set('mobileOpen', true);
+  }
+
+  @action
+  closeNav(){
+    this.set('mobileOpen', false);
+  }
+
+}
