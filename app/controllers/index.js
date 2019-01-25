9d98dce07b37440f065014f49156f6836bf7410e
Aad Versteden <madnificent@gmail.com>
2019-01-25 12:55:16 +0100

    Use ember-anchor to link to contact
    The contact link should scroll to the bottom of the about page.  This
    wasn't implemented yet as we're in a SPA world.  The ember-anchor
    helps us jump to the right position in the page.

---
+++ app/controllers/index.js
@@ -0,0 +1,5 @@
+import Controller from '@ember/controller';
+import AnchorControllerMixin from 'ember-anchor/mixins/controller-support';
+
+export default class IndexController extends Controller.extend( AnchorControllerMixin ) {
+}
