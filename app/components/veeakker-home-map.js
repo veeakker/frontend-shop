970ec4807561477f3d874415071067c06d2f6925
Aad Versteden <madnificent@gmail.com>
2019-01-28 10:48:52 +0100

    More documentation in veeakker-home-map component
    The veeakker-home-map component is somewhat clear.  We've (over)
    documented this as it's likely something we'll see again only in a
    distant time.  It's nice to have overdocumented this in that case, as
    it will help us update the configuration at that time.

--- app/components/veeakker-home-map.js
+++ app/components/veeakker-home-map.js
@@ -3,9 +3,14 @@ import Component from '@ember/component';
 export default class VeeakkerHomeMapComponent extends Component {
   classNames = ['veeakker-home-map__leaflet-map']
 
-  lat = 50.874007
-  lng = 4.689850
-  zoom = 12
+  // Configuration of butchery location
+  // (popup marker content is in the template)
+  butcheryLat = 50.874007
+  butcheryLng = 4.689850
+  defaultZoom = 12
 
-  veeakkerButchery = [50.874007, 4.689850]
+  init() {
+    super.init(...arguments);
+    this.set('veeakkerButcheryLocation', [this.butcheryLat, this.butcheryLng]);
+  }
 }
