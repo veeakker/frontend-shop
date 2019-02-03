6b8c3ad8e401ac6d8e20be7bb6c0f4ddfe310ba0
Aad Versteden <madnificent@gmail.com>
2019-02-03 22:11:24 +0100

    Implementation with varying icons for varying contents
    The icons are taken from a leaflet tutorial.  We will need different
    icons for the various types of locations.

--- app/components/delivery-locations-map.js
+++ app/components/delivery-locations-map.js
@@ -8,4 +8,18 @@ export default class DeliveryLocationsMapComponent extends Component {
   baseLat = 50.874007;
   baseLng = 4.689850;
   baseZoom = 9;
+
+
+  // leaf icon settings
+  iconSettings = {
+    iconUrl: '/images/leaf-green.png',
+    shadowUrl: '/images/leaf-shadow.png',
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+  }
+  orangeLeafUrl = '/images/leaf-orange.png';
+  redLeafUrl = '/images/leaf-red.png';
 }
--- app/models/delivery-kind.js
+++ app/models/delivery-kind.js
@@ -1,9 +1,27 @@
 import DS from 'ember-data';
 const { Model } = DS;
 import { attr, hasMany } from '@ember-decorators/data';
+import { computed } from '@ember-decorators/object';
 
 export default class DeliveryKindModel extends Model {
   @attr() label;
   @attr() uri;
   @hasMany('delivery-place') deliveryPlaces;
+
+  @computed('uri')
+  get simpleName(){
+    const uri = this.uri;
+    if( uri == "http://veeakker.be/delivery-kinds/winkel" )
+      return "winkel";
+    if( uri == "http://veeakker.be/delivery-kinds/toeren")
+      return "toer";
+    if( uri == "http://veeakker.be/delivery-kinds/natuurwinkels")
+      return "natuurwinkel";
+    if( uri == "http://veeakker.be/delivery-kinds/buurderijen")
+      return "buurderij";
+    if( uri == "http://veeakker.be/delivery-kinds/webshop")
+      return "webshop";
+    else
+      return "onbekend";
+  }
 }
