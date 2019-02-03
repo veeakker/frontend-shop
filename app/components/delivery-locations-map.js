126cea0a39a5fa2adb6b80a0364ba85a9e057d8a
Aad Versteden <madnificent@gmail.com>
2019-02-03 21:35:27 +0100

    Abstract locations into separate component
    Locations are now handeld in a separate component.  This component
    simply renders the map with its default contents.  Further information
    should be supplied from within this component.

---
+++ app/components/delivery-locations-map.js
@@ -0,0 +1,11 @@
+import Component from '@ember/component';
+
+export default class DeliveryLocationsMapComponent extends Component {
+  // configuration
+  tagName = "";
+
+  // default values
+  baseLat = 50.874007;
+  baseLng = 4.689850;
+  baseZoom = 9;
+}
