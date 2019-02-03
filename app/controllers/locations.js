4a811fc52cbf6d0ba3cc1de4361a94a8c6df8b84
Aad Versteden <madnificent@gmail.com>
2019-02-03 21:25:21 +0100

    Replace static map on site with dynamic map
    The data generated in the backend can now render a map with basic
    contents.  When contents are not ready to be loaded, a base definition
    is loaded for testing purposes.  This map will be abstracted further.

---
+++ app/controllers/locations.js
@@ -0,0 +1,5 @@
+import Controller from '@ember/controller';
+
+export default class LocationsController extends Controller {
+  butcheryLocation = [50.874007,4.689850];
+}
--- app/routes/locations.js
+++ app/routes/locations.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember-decorators/service';
 
 export default class LocationsRoute extends Route {
+  @service store;
+
+  model(){
+    if( this.store.peekAll('delivery-place') ){
+      // fetches when it needs to be refreshed or is incomplete.
+      return this.store.findAll('delivery-place');
+    } else {
+      return this.store.query('delivery-place', { include: "delivery-kind,geo-coordinate.postal-address,postal-address", limit: 250 });
+    }
+  }
 }
