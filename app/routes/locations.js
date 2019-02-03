2d91d8349aa85dbd56390af8fc92caae254c381b
Aad Versteden <madnificent@gmail.com>
2019-02-03 22:14:38 +0100

    Always fetch complete locations
    The amount of locations received might not be complete.  In order to
    ensure we have the full list, we now fetch them all on every call.
    
    There are some optimizations which can be made in order to ensure we
    have the full contents without fetching the data upon each request.
    We'll leave that for later.

--- app/routes/locations.js
+++ app/routes/locations.js
@@ -5,11 +5,6 @@ export default class LocationsRoute extends Route {
   @service store;
 
   model(){
-    if( this.store.peekAll('delivery-place') ){
-      // fetches when it needs to be refreshed or is incomplete.
-      return this.store.findAll('delivery-place');
-    } else {
-      return this.store.query('delivery-place', { include: "delivery-kind,geo-coordinate.postal-address,postal-address", limit: 250 });
-    }
+    return this.store.query('delivery-place', { include: "delivery-kind,geo-coordinate.postal-address,postal-address", "page[size]": 250 });
   }
 }
