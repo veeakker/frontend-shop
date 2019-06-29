e57ae52813b56b407e54dda9948592ecc9d48413
Aad Versteden <madnificent@gmail.com>
2019-06-29 19:06:32 +0200

    Clear offer when revisiting the product detail route
    The state is remembered in the controller which does not reset.  As
    such, the original state is available when details are being visited.
    
    There is still an issue with the selectedOffer seemingly not being
    taken into account.  This is a strange behaviour.

--- app/routes/webshop/product.js
+++ app/routes/webshop/product.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default class WebshopProductRoute extends Route {
+  setupController( controller, model ) {
+    super.setupController( ...arguments );
+    controller.resetOrder();
+  }
 }
