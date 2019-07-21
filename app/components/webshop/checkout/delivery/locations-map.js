723289110a98d211193ed7e7bce84d37df9ad578
Aad Versteden <madnificent@gmail.com>
2019-07-21 14:54:26 +0200

    Update locations map for delivery methods
    This is a leftover commit.  It belongs to the actions that render a
    location map for users to select the place where they want to pick up
    their order, on checkout.  This same map could be rendered when
    letting users select their default location.

--- app/components/webshop/checkout/delivery/locations-map.js
+++ app/components/webshop/checkout/delivery/locations-map.js
@@ -1,7 +1,8 @@
-import { action } from '@ember/object';
+import { defineProperty } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 import { uriForNormalizedLabel } from 'veeakker/models/delivery-kind';
+import { task } from 'ember-concurrency';
 
 export default class WebshopCheckoutDeliveryLocationsMap extends Component {
   @service store
@@ -10,33 +11,38 @@ export default class WebshopCheckoutDeliveryLocationsMap extends Component {
 
   shownLocationName = null;
 
+  init(){
+    super.init(...arguments);
+    // TODO: migrate to ember-concurrency-decorator once that's in a
+    // stable and usable state.
+    defineProperty( this, 'fetchLocations', task( function*(){
+      const allPlaces = yield this.store.loadRecords('delivery-place', {
+        include: "delivery-kind,geo-coordinate.postal-address,postal-address",
+        "page[size]": 250
+      });
+
+      let allowedTypes = [];
+      // TODO: is this split correct? first is pickup when we want, the
+      // other is when they want.
+      if( this.shownLocationName == "tour" ){
+        allowedTypes = [uriForNormalizedLabel( "routes" ),
+                        uriForNormalizedLabel( "local-farms" )];
+      } else if( this.shownLocationName == "shop" ) {
+        allowedTypes = [uriForNormalizedLabel( "health-shops" )];
+      }
+
+      this.set(
+        'locations',
+        allPlaces.filter(
+          (place) => allowedTypes.includes( place.get('deliveryKind.uri') ) ) );
+    } ).restartable() );
+  }
+
   didReceiveAttrs(){
     if( this.locationName != this.shownLocationName ) {
       this.shownLocationName = this.locationName;
-      this.fetchLocations();
+      this.fetchLocations.perform();
     }
   }
 
-  @action
-  async fetchLocations() {
-    const allPlaces = await this.store.loadRecords('delivery-place', {
-      include: "delivery-kind,geo-coordinate.postal-address,postal-address",
-      "page[size]": 250
-    });
-
-    let allowedTypes = [];
-    // TODO: is this split correct? first is pickup when we want, the
-    // other is when they want.
-    if( this.shownLocationName == "tour" ){
-      allowedTypes = [uriForNormalizedLabel( "routes" ),
-                      uriForNormalizedLabel( "local-farms" )];
-    } else if( this.shownLocationName == "shop" ) {
-      allowedTypes = [uriForNormalizedLabel( "health-shops" )];
-    }
-
-    this.set(
-      'locations',
-      allPlaces.filter(
-        (place) => allowedTypes.includes( place.get('deliveryKind.uri') ) ) );
-  }
 }
