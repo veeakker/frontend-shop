a3b3a6855775e8870f56631f7e88f3127360bfa6
Aad Versteden <madnificent@gmail.com>
2019-07-21 14:55:22 +0200

    Frontend implementation for using the image service
    The image service can be used to fetch images with the right
    resolution.  This is a way of embedding the service in the frontend.
    We notice that we have to execute a bunch of workarounds in order to
    make the service work this way.  As such we'll likely go back to the
    drawing board and update this code so it's much less hackish and
    easier to read.
    
    There are a few solutions.  The most common pattern is likely to drop
    this logic into the model with a sensible set of default sizes which
    can be used throughout the application.  That has its downsides too,
    but it would be easier to read.  We can further abstract the
    calculation and offer it as a separate method for other users.

--- app/components/product-card/thumbnail.js
+++ app/components/product-card/thumbnail.js
@@ -1,8 +1,22 @@
+import { computed } from '@ember/object';
 import { alias } from '@ember/object/computed';
 import Component from '@ember/component';
 
 export default class extends Component {
   tagName = ""
 
-  @alias( "product.thumbnail.downloadUrl" ) downloadUrl
+  @computed('product.thumbnail.id')
+  get imageUrl() {
+    // this.get('product.thumbnail.downloadUrl'); // invalidate keys
+    const size = { width: 244, height: 200 };
+    const thumbnail = this.get('product.thumbnail');
+    if( thumbnail && thumbnail.get('id') ) {
+      const thumbnailObj = thumbnail.sizedImageUrl ? thumbnail : thumbnail.content;
+      if( thumbnail.content )
+        return thumbnail.content.sizedImageUrl( size );
+    }
+
+    return null;
+  }
+  // @alias( "product.thumbnail.downloadUrl" ) downloadUrl
 }
--- app/models/file.js
+++ app/models/file.js
@@ -11,4 +11,21 @@ export default class FileModel extends Model {
       return `/files/${this.get('id')}/download`;
     return undefined;
   }
+
+  sizedImageUrl( { width = null, height = null } ){
+    const params = { width, height };
+
+    // build query params string
+    var esc = encodeURIComponent;
+    var query = Object.keys(params)
+        .filter( k => params[k] !== null )
+        .map(k => esc(k) + '=' + esc(params[k]))
+        .join('&');
+
+    // build the resulting uri
+    if( this.get('id') )
+      return `/images/${this.get('id')}?${query}`;
+
+    return undefined;
+  }
 }
