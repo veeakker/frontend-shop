3b7a1db2360ea903730a2050d5b31503f8ced3fc
Aad Versteden <madnificent@gmail.com>
2020-05-24 15:40:21 +0200

    Rendering product-labels in the Product card
    Whilst we were at it, we converted the component to be a Glimmer
    component as that is slightly nicer to read.  That showed a few issues
    in our current conversion process.  Yay for finding these future
    issues early on.

--- app/components/product-card.js
+++ app/components/product-card.js
@@ -1,9 +1,7 @@
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
-import { computed } from '@ember/object';
-import { alias } from '@ember/object/computed';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 // import { wait } from 'ember-animated';
 
 const wait = function( time ) {
@@ -23,15 +21,43 @@ export default class ProductCardComponent extends Component {
 
   @service basket
 
-  @alias('product.sortedOfferings.firstObject')
-  firstOffer;
+  get firstOffer() {
+    return this.args.product && this.args.product.sortedOfferings.firstObject;
+  }
 
-  @computed('firstOffer.id', 'selectedOffer', 'selectedOffer.id')
+  get labelArray() {
+    const enabled = (this.args.product && this.args.product.productLabels || []);
+    return [{ uri: "http://veeakker.be/product-labels/d9fa5ad6-0d0e-4990-b8a7-ca3a60eb3a85",
+        label: "Frozen",
+        image: "/images/product-labels/diepvries.png",
+        selected: enabled.includes( "http://veeakker.be/product-labels/d9fa5ad6-0d0e-4990-b8a7-ca3a60eb3a85")
+      },
+      { uri: "http://veeakker.be/product-labels/fa0d5d40-762e-4f89-ac82-46c1a4ee00bf",
+        label: "Natuurpunt",
+        image: "/images/product-labels/natuurpunt.png",
+        selected: enabled.includes( "http://veeakker.be/product-labels/fa0d5d40-762e-4f89-ac82-46c1a4ee00bf")
+      },
+      { uri: "http://veeakker.be/product-labels/c9e43e38-3f7f-4116-9817-80bdede3f123",
+        label: "PintaFish",
+        image: "/images/product-labels/pintafish.png",
+        selected: enabled.includes( "http://veeakker.be/product-labels/c9e43e38-3f7f-4116-9817-80bdede3f123")
+      }];
+  }
+
+  get enabledLabelArray() {
+    return this.labelArray.filter(({selected}) => selected);
+  }
+
   get currentOffer(){
     // It is not clear why we have to base ourselves on the id
     // property in this case, but that seems to make it work.
-    this.get('firstOffer.id'); this.get('selectedOffer.id');
-    return this.selectedOffer || this.firstOffer;
+    if( this.selectedOffer && this.selectedOffer.id ){
+      return this.selectedOffer;
+    }
+    if( this.firstOffer && this.firstOffer.id ){
+      return this.firstOffer;
+    }
+    return ;
   }
 
   get detailClass() {
