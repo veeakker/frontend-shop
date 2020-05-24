3b2b87df33a9d9a6da957b44001daedf6c422818
Aad Versteden <madnificent@gmail.com>
2020-05-24 15:47:36 +0200

    Moving label calculation into product model
    The label calculation occurs in multiple places.  It's better suited
    for the product model itself so it can more readily be shared.

--- app/components/product-card.js
+++ app/components/product-card.js
@@ -25,29 +25,6 @@ export default class ProductCardComponent extends Component {
     return this.args.product && this.args.product.sortedOfferings.firstObject;
   }
 
-  get labelArray() {
-    const enabled = (this.args.product && this.args.product.productLabels || []);
-    return [{ uri: "http://veeakker.be/product-labels/d9fa5ad6-0d0e-4990-b8a7-ca3a60eb3a85",
-        label: "Frozen",
-        image: "/images/product-labels/diepvries.png",
-        selected: enabled.includes( "http://veeakker.be/product-labels/d9fa5ad6-0d0e-4990-b8a7-ca3a60eb3a85")
-      },
-      { uri: "http://veeakker.be/product-labels/fa0d5d40-762e-4f89-ac82-46c1a4ee00bf",
-        label: "Natuurpunt",
-        image: "/images/product-labels/natuurpunt.png",
-        selected: enabled.includes( "http://veeakker.be/product-labels/fa0d5d40-762e-4f89-ac82-46c1a4ee00bf")
-      },
-      { uri: "http://veeakker.be/product-labels/c9e43e38-3f7f-4116-9817-80bdede3f123",
-        label: "PintaFish",
-        image: "/images/product-labels/pintafish.png",
-        selected: enabled.includes( "http://veeakker.be/product-labels/c9e43e38-3f7f-4116-9817-80bdede3f123")
-      }];
-  }
-
-  get enabledLabelArray() {
-    return this.labelArray.filter(({selected}) => selected);
-  }
-
   get currentOffer(){
     // It is not clear why we have to base ourselves on the id
     // property in this case, but that seems to make it work.
--- app/models/product.js
+++ app/models/product.js
@@ -16,4 +16,27 @@ export default class ProductModel extends Model {
 
   @sort('offerings', 'offeringSortKeys') sortedOfferings;
   offeringSortKeys = ['typeAndQuantity.value']
+
+  get labelArray() {
+    const enabled = (this.productLabels || []);
+    return [{ uri: "http://veeakker.be/product-labels/d9fa5ad6-0d0e-4990-b8a7-ca3a60eb3a85",
+        label: "Frozen",
+        image: "/images/product-labels/diepvries.png",
+        selected: enabled.includes( "http://veeakker.be/product-labels/d9fa5ad6-0d0e-4990-b8a7-ca3a60eb3a85")
+      },
+      { uri: "http://veeakker.be/product-labels/fa0d5d40-762e-4f89-ac82-46c1a4ee00bf",
+        label: "Natuurpunt",
+        image: "/images/product-labels/natuurpunt.png",
+        selected: enabled.includes( "http://veeakker.be/product-labels/fa0d5d40-762e-4f89-ac82-46c1a4ee00bf")
+      },
+      { uri: "http://veeakker.be/product-labels/c9e43e38-3f7f-4116-9817-80bdede3f123",
+        label: "PintaFish",
+        image: "/images/product-labels/pintafish.png",
+        selected: enabled.includes( "http://veeakker.be/product-labels/c9e43e38-3f7f-4116-9817-80bdede3f123")
+      }];
+  }
+
+  get enabledLabelArray() {
+    return this.labelArray.filter(({selected}) => selected);
+  }
 }
