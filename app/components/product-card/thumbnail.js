907877e067c7307f2feed97a1d7b314ecb133b30
Aad Versteden <madnificent@gmail.com>
2021-02-27 18:25:05 +0100

    Progressive enhancement of image quality in detail view
    If we've already have a lower quality image available, fetched from the
    overview page, then we'll first render than and fetch a higher
    resolution image in the background.

--- app/components/product-card/thumbnail.js
+++ app/components/product-card/thumbnail.js
@@ -12,7 +12,28 @@ export default class extends Component {
       if( thumbnail.content )
         return thumbnail.content.sizedImageUrl( size );
     }
+    return null;
+  }
 
+  @computed('product.thumbnail.id')
+  get largeImageUrl() {
+    const size = { width: 500, height: 410 };
+    const thumbnail = this.product && this.product.get("thumbnail");
+    if( thumbnail && thumbnail.get('id') ) {
+      if( thumbnail.content )
+        return thumbnail.content.sizedImageUrl( size );
+    }
+    return null;
+  }
+
+  @computed('product.thumbnail.id')
+  get doubleLargeImageUrl() {
+    const size = { width: 1000, height: 820 };
+    const thumbnail = this.product && this.product.get("thumbnail");
+    if( thumbnail && thumbnail.get('id') ) {
+      if( thumbnail.content )
+        return thumbnail.content.sizedImageUrl( size );
+    }
     return null;
   }
 }
