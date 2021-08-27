77f18b4efbda587002221674f94c1b8c43883b1b
Siebe Dreesen <siebedreesen@icloud.com>
2021-08-27 12:46:08 +0200

    Foundation for authentication system

--- app/controllers/webshop.js
+++ app/controllers/webshop.js
@@ -2,6 +2,7 @@ import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class WebshopController extends Controller {
   // @sort( "model", ["sortIndex"] ) productGroups;
@@ -12,5 +13,14 @@ export default class WebshopController extends Controller {
 
   @computed( "model.@each.sortIndex" ) get sortedProductGroups() {
     return this.model.sortBy('sortIndex');
+  }
+
+  @action 
+  async logout() {
+    try {
+      await this.session.invalidate('authenticator:mu-semtech');
+    } catch(err){
+      this.error = err.errors[0].detail;
+    }
   }
 }
