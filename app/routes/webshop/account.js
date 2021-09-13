0e997019805f7e9a21b634e4598883b0257e2b5c
Siebe Dreesen <siebedreesen@icloud.com>
2021-09-13 17:37:53 +0100

    BUG authorization

--- app/routes/webshop/account.js
+++ app/routes/webshop/account.js
@@ -1,4 +1,16 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class WebshopAccountRoute extends Route {
+  @service() session;
+
+  beforeModel(transition) {
+    this.session.requireAuthentication(transition, 'login');
+  }
+
+  async model(params){
+    console.log(this.session.data.authenticated.relationships.account.data.id)
+    debugger;
+    return await this.store.findRecord('account', this.session.data.authenticated.relationships.account.data.id);
+  }
 }
