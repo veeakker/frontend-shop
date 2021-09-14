7dc3892f2a1c151a52c02c16577507a9eac279cb
Siebe Dreesen <siebedreesen@icloud.com>
2021-09-14 17:05:12 +0100

    BUG undefined name

--- app/models/account.js
+++ app/models/account.js
@@ -2,7 +2,5 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 export default class AccountModel extends Model {
   @attr('string') email;
   @attr('string') password;
-  @attr('string') firstName;
-  @attr('string') lastName;
-  @belongsTo('postal-address') postalAddres;
+  @belongsTo('person', {inverse: 'accounts'}) person;
 }
