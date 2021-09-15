8252374394cfe0f4a99fc161be2b8730863b3b8e
Siebe Dreesen <siebedreesen@icloud.com>
2021-09-15 13:25:59 +0100

    Account works with address

---
+++ app/controllers/webshop/account.js
@@ -0,0 +1,12 @@
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import Controller from '@ember/controller';
+
+export default class WebshopAccountRoute extends Controller {
+  
+  // async getAddress(personId) {
+  //   debugger;
+  //   const person = await this.store.findRecord('person', personId, {include: "postalAddress"});
+  //   return person.postalAddress
+  // }
+}
--- app/models/person.js
+++ app/models/person.js
@@ -3,6 +3,6 @@ import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 export default class PersonModel extends Model {
   @attr('string') firstName;
   @attr('string') lastName;
-  // @belongsTo('postal-address') postalAddres;
+  @belongsTo('postal-address') postalAddress;
   @hasMany('account') accounts;
 }
