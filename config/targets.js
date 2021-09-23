17c69bed92bf3bde713ee7ad1f91c4fbb7131c05
Aad Versteden <madnificent@gmail.com>
2021-09-23 12:52:59 +0100

    WIP product cards

--- config/targets.js
+++ config/targets.js
@@ -1,8 +1,8 @@
 'use strict';
 
 let browsers = [
-  'last 2 Chrome versions',
-  'last 2 Firefox versions',
+  'last 1 Chrome versions',
+  'last 1 Firefox versions',
   'last 1 Safari versions'
 ];
 
