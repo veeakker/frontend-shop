4e2344c42ad1be6c36ddfb7917fad70678791a1d
Aad Versteden <madnificent@gmail.com>
2022-10-18 20:47:32 +0100

    Allow for multiple parent groups in the data model

--- app/models/product-group.js
+++ app/models/product-group.js
@@ -3,7 +3,7 @@ import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 export default class ProductGroupModel extends Model {
   @attr() label;
   @attr('number') sortIndex;
-  @belongsTo('product-group', { inverse: "childGroups" }) parentGroup;
+  @hasMany('product-group', { inverse: "childGroups" }) parentGroups;
   @hasMany('product-group', { inverse: "parentGroup" }) childGroups;
   @hasMany('spotlight-product') spotlightProducts;
   @hasMany('product', { inverse: "productGroups" }) products;
--- app/routes/webshop.js
+++ app/routes/webshop.js
@@ -5,6 +5,6 @@ export default class WebshopRoute extends Route {
   @service store;
 
   model() {
-    return this.store.query('product-group', { "filter[:has-no:parent-group]": "yes" });
+    return this.store.query('product-group', { "filter[:has-no:parent-groups]": "yes" });
   }
 }
