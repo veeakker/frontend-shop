2637ed70eafbc31881a62fc500a740cd6b4a2196
Aad Versteden <madnificent@gmail.com>
2022-10-18 20:47:48 +0100

    Remove some deprecation warnings

--- app/routes/webshop/product.js
+++ app/routes/webshop/product.js
@@ -1,6 +1,9 @@
+import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 
 export default class WebshopProductRoute extends Route {
+  @service store
+
   model(params){
     return this.store.loadRecord('product', params["product_id"], {
       include: "offerings.unit-price,offerings.type-and-quantity"
