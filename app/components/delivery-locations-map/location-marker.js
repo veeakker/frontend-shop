ee0e6ee43206b17f018a63138bd662f826db4d67
van Daele Karel (s129128) <karel.v.daele200@gmail.com>
2022-10-25 16:26:56 +0100

    legende-toegevoegt

--- app/components/delivery-locations-map/location-marker.js
+++ app/components/delivery-locations-map/location-marker.js
@@ -7,29 +7,29 @@ export default class DeliveryLocationsMapLocationMarkerComponent extends Compone
   @tracked
   allIconOptions = {
     greenLeaf: {
-      iconUrl: '/images/leaf-green.png',
-      shadowUrl: '/images/leaf-shadow.png',
-      iconSize:     [38, 95], // size of the icon
+      iconUrl: '/images/van.png',
+      //shadowUrl: '/images/leaf-shadow.png',
+      iconSize:     [48, 48], // size of the icon
       shadowSize:   [50, 64], // size of the shadow
-      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      iconAnchor:   [20, 20], // point of the icon which will correspond to marker's location
       shadowAnchor: [4, 62],  // the same for the shadow
       popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
     },
     orangeLeaf: {
-      iconUrl: '/images/leaf-orange.png',
-      shadowUrl: '/images/leaf-shadow.png',
-      iconSize:     [38, 95], // size of the icon
+      iconUrl: '/images/shop.png',
+      //shadowUrl: '/images/leaf-shadow.png',
+      iconSize:     [40, 40], // size of the icon
       shadowSize:   [50, 64], // size of the shadow
-      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      iconAnchor:   [20, 20], // point of the icon which will correspond to marker's location
       shadowAnchor: [4, 62],  // the same for the shadow
       popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
     },
     redLeaf: {
-      iconUrl: '/images/leaf-red.png',
-      shadowUrl: '/images/leaf-shadow.png',
-      iconSize:     [38, 95], // size of the icon
+      iconUrl: '/images/barn.png',
+      //shadowUrl: '/images/leaf-shadow.png',
+      iconSize:     [40, 40], // size of the icon
       shadowSize:   [50, 64], // size of the shadow
-      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      iconAnchor:   [20, 20], // point of the icon which will correspond to marker's location
       shadowAnchor: [4, 62],  // the same for the shadow
       popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
     }
