da2da765b1c83a89af433f08be873ec524ea0306
Aad Versteden <madnificent@gmail.com>
2022-12-06 15:54:47 +0100

    Fix name of inverse relationship of product-group

--- app/models/product-group.js
+++ app/models/product-group.js
@@ -4,7 +4,7 @@ export default class ProductGroupModel extends Model {
   @attr() label;
   @attr('number') sortIndex;
   @hasMany('product-group', { inverse: "childGroups" }) parentGroups;
-  @hasMany('product-group', { inverse: "parentGroup" }) childGroups;
+  @hasMany('product-group', { inverse: "parentGroups" }) childGroups;
   @hasMany('spotlight-product') spotlightProducts;
   @hasMany('product', { inverse: "productGroups" }) products;
 }
