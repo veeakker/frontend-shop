274f0c79b805c54e82c01cbbec3bb75de74d0f26
Aad Versteden <madnificent@gmail.com>
2023-01-23 15:48:54 +0100

    Allow entering contact address
    This is the entering and persisting of the address, not something
    regarding knowing which address to select.  There is also no requirement
    to enter all of this information.  We should verify if we need all (or
    any) of it.

---
+++ app/models/full-address.js
@@ -0,0 +1,33 @@
+import { set } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import Model, { attr, belongsTo } from '@ember-data/model';
+import { use, Resource } from 'ember-could-get-used-to-this';
+
+class EnsureAddressResource extends Resource {
+  @tracked value = null;
+  @service store;
+
+  async setup() {
+    const address = this.args.positional[0];
+    if (await address.postalAddress)
+      this.value = await address.postalAddress;
+    else
+      this.value = set(address,"postalAddress", this.store.createRecord('postal-address'));
+  }
+}
+
+export default class FullAddressModel extends Model {
+  @attr('string') name;
+  @attr('string') company;
+  @attr('string') telephone;
+  @belongsTo('postal-address') postalAddress;
+
+  @use
+  address = new EnsureAddressResource(() => [this]);
+
+  async deepPersist() {
+    await this.address?.save();
+    await this.save();
+  }
+}
