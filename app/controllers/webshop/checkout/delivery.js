39f0230034c5762c92e81cc2ee76310f875082f0
Aad Versteden <madnificent@gmail.com>
2023-01-23 18:43:21 +0100

    Custom delivery for basket
    Basket now understands where something should be delivered with extra
    metadata, remembering what the user has entered.

--- app/controllers/webshop/checkout/delivery.js
+++ app/controllers/webshop/checkout/delivery.js
@@ -4,9 +4,25 @@ import Controller from '@ember/controller';
 
 export default class WebshopCheckoutController extends Controller {
   @service basket
-  @tracked deliveryMethod;
+  @tracked currentDeliveryMethod;
 
-  get payDirectly(){
+  get payDirectly() {
     return this.deliveryMethod == "postal";
+  }
+
+  get deliveryMethod() {
+    return this.currentDeliveryMethod;
+  }
+
+  set deliveryMethod(method) {
+    // postal tour shop false
+    this.currentDeliveryMethod = method;
+
+    if (method)
+      this.basket.basket.deliveryType = `http://veeakker.be/delivery-methods/${method}`;
+    else
+      this.basket.basket.deliveryType = null;
+
+    this.basket.basket.save();
   }
 }
