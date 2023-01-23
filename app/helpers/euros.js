d529261cc3942f70ce694321bb4c7c76398aa663
Aad Versteden <madnificent@gmail.com>
2023-01-23 22:21:26 +0100

    Remove spurious logging for euros

--- app/helpers/euros.js
+++ app/helpers/euros.js
@@ -1,7 +1,6 @@
 import { helper } from '@ember/component/helper';
 
 export default helper(function euros([ amount ]/*, hash*/) {
-  console.log( { amount } );
   if( amount && amount !== NaN ) {
     return `€ ${amount.toFixed(2)}`;
   } else {
