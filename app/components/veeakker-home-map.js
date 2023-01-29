b22a996766cb241c6c6dcb49b29d0db8cb15791e
Aad Versteden <madnificent@gmail.com>
2023-01-29 15:54:04 +0100

    Update addresses

--- app/components/veeakker-home-map.js
+++ app/components/veeakker-home-map.js
@@ -4,8 +4,9 @@ import Component from '@glimmer/component';
 export default class VeeakkerHomeMapComponent extends Component {
   // Configuration of butchery location
   // (popup marker content is in the template)
-  butcheryLat = 50.874007
-  butcheryLng = 4.689850
+
+  butcheryLat = 50.79006
+  butcheryLng = 4.93802
   defaultZoom = 12
 
   @tracked veeakkerButcheryLocation;
