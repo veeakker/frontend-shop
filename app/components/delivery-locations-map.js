a580b794d9adfa014c88156eb20424d0a1da9ebd
Aad Versteden <madnificent@gmail.com>
2023-02-19 14:10:06 +0100

    Remove linting warnings

--- app/components/delivery-locations-map.js
+++ app/components/delivery-locations-map.js
@@ -1,9 +1,6 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
 export default class DeliveryLocationsMapComponent extends Component {
-  // configuration
-  tagName = "";
-
   // default values
   baseLat = 50.874007;
   baseLng = 4.689850;
