4386e4fed25566c24dd9496b23c784a053436441
Aad Versteden <madnificent@gmail.com>
2023-02-19 17:05:50 +0100

    Use logged in user for default ordering address

--- app/routes/webshop/checkout/contact-info.js
+++ app/routes/webshop/checkout/contact-info.js
@@ -4,9 +4,20 @@ import Route from '@ember/routing/route';
 
 export default class WebshopContactInfoRoute extends Route {
   @service basket;
+  @service session;
 
   @action
   async willTransition(_transition) {
     this.basket.basket.deepPersist();
+  }
+
+  async activate() {
+    if( this.session.isAuthenticated ) {
+      // TODO: DRY with webshop/account
+      const account = await this.store.findRecord('account', this.session.data.authenticated.relationships.account.data.id, {include: "person"});
+      this.basket.basket.customer = account.person;
+      await this.basket.basket.save();
+      this.transitionTo("webshop.checkout.delivery");
+    }
   }
 }
