4dbba84f5a8041c9b4a8830cb10ef0a51ec99df4
Aad Versteden <madnificent@gmail.com>
2023-02-25 12:35:50 +0100

    Move to editing mode of account immediately.
    It probably doesn't matter too much in this regard.

--- app/controllers/webshop/account.js
+++ app/controllers/webshop/account.js
@@ -1,12 +1,9 @@
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 import Controller from '@ember/controller';
 
 export default class WebshopAccountController extends Controller {
-  @tracked editing = false;
-
   @action
-  async finishEditing(event) {
+  async save(event) {
     event.preventDefault();
     const account = this.model;
     // await account.save(); // TODO: save the account and related info through custom service
@@ -14,7 +11,5 @@ export default class WebshopAccountController extends Controller {
     await person.save();
     const postal = await person.postalAddress;
     await postal.save();
-
-    this.editing = false;
   }
 }
