846460d41dd574ac72eefc8fd7f5aaf9dacea427
Aad Versteden <madnificent@gmail.com>
2023-02-26 11:33:52 +0100

    Finish basket through custom service

--- app/routes/webshop/checkout/finish.js
+++ app/routes/webshop/checkout/finish.js
@@ -1,14 +1,7 @@
-import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 
 export default class WebshopCheckoutFinishRoute extends Route {
-  @service store;
-
   async model({basket_id}) {
-    const baskets = await this.store.query('basket', {
-      "filter[:id:]": basket_id,
-      include: "order-lines.offering.type-and-quantity.product,order-lines.offering.unit-price,delivery-address.postal-address,invoice-address.postal-address"
-    });
-    return baskets.firstObject;
+    return basket_id;
   }
 }
