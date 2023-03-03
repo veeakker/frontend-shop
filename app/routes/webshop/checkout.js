742cf9bf02613260f2e2360a31b540b175503f6a
Aad Versteden <madnificent@gmail.com>
2023-03-03 17:48:04 +0100

    Render checkout map close to selected location
    This also implied loading the basket before we enter these rendering
    loops.  The approach should make it easier to manipulate the basket as
    an end-user.

--- app/routes/webshop/checkout.js
+++ app/routes/webshop/checkout.js
@@ -1,4 +1,10 @@
+import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 
 export default class WebshopCheckoutRoute extends Route {
+  @service basket
+
+  model() {
+    return this.basket.pBasket;
+  }
 }
