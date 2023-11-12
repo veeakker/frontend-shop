488df185c6b5e645eb11b185b182dd2cde13d278
Aad Versteden <madnificent@gmail.com>
2023-11-12 12:33:27 +0100

    Remove unused code
    These sections have been refactored.  Better remove them to reduce confusion.

--- app/models/basket.js
+++ app/models/basket.js
@@ -1,23 +1,4 @@
-import { set } from '@ember/object';
-import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
-import { get } from '@ember/object';
 import Model, { hasMany, attr, belongsTo } from '@ember-data/model';
-import { use, Resource } from 'ember-could-get-used-to-this';
-
-class EnsureAddressResource extends Resource {
-  @tracked value = null;
-  @service store;
-
-  async setup() {
-    const basket = this.args.positional[0];
-    const property = this.args.positional[1];
-    if ( await get(basket, property) )
-      this.value = await get(basket, property);
-    else
-      this.value = set(basket, property, this.store.createRecord('full-address'));
-  }
-}
 
 export default class BasketModel extends Model {
   @attr() paymentStatus;
@@ -38,16 +19,4 @@ export default class BasketModel extends Model {
   get invoice() {
     return this.invoiceAddress;
   }
-
-  // @use
-  // delivery = new EnsureAddressResource(() => [this, "deliveryAddress"]);
-
-  // @use
-  // invoice = new EnsureAddressResource(() => [this, "invoiceAddress"]);
-
-  // async deepPersist() {
-  //   await this.delivery?.deepPersist();
-  //   await this.invoice?.deepPersist();
-  //   await this.save();
-  // }
 }
