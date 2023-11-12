6dfd666e37924c416f67970d63f9b1638f48641c
Aad Versteden <madnificent@gmail.com>
2023-11-12 12:35:38 +0100

    Support rendering of enabled locations

--- app/controllers/locations.js
+++ app/controllers/locations.js
@@ -2,4 +2,8 @@ import Controller from '@ember/controller';
 
 export default class LocationsController extends Controller {
   butcheryLocation = [50.874007,4.689850];
+
+  get enabledLocations() {
+    return this.model?.filter( (e) => e.isEnabled );
+  }
 }
