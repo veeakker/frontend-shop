a4ea3fd433127347ccb39bae90499e38798485b8
Aad Versteden <madnificent@gmail.com>
2024-05-23 09:56:42 +0200

    Work on delivery and account edit

--- app/controllers/webshop/account.js
+++ app/controllers/webshop/account.js
@@ -1,15 +1,24 @@
+import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import Controller from '@ember/controller';
 
 export default class WebshopAccountController extends Controller {
+  @tracked saved = false
+
   @action
   async save(event) {
     event.preventDefault();
     const account = this.model;
-    // await account.save(); // TODO: save the account and related info through custom service
+    await account.save();
     const person = await account.person;
     await person.save();
     const postal = await person.postalAddress;
     await postal.save();
+    this.saved = true;
+  }
+
+  activate() {
+    alert("yo");
+    this.saved = false;
   }
 }
--- app/models/account.js
+++ app/models/account.js
@@ -1,6 +1,7 @@
+import { tracked } from '@glimmer/tracking';
 import Model, { attr, belongsTo } from '@ember-data/model';
 export default class AccountModel extends Model {
   @attr('string') email;
-  @attr('string') password;
+  @tracked password;
   @belongsTo('person', {inverse: 'accounts'}) person;
 }
