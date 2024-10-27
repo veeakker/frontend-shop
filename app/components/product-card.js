ee3eda357bd9e917f3c416084e8f6c74187a98a9
Aad Versteden <madnificent@gmail.com>
2024-10-27 20:53:41 +0100

    Make product card in overview go from g to kg
    Product Card now sorts the amounts between G and KG in one go.

--- app/components/product-card.js
+++ app/components/product-card.js
@@ -5,6 +5,19 @@ import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { use, Resource } from 'ember-could-get-used-to-this';
 import wait from '../utils/wait';
+import OfferingModel from '../models/offering';
+
+const UNIT_TO_CODE = {
+  "st": "C62",
+  "kg": "KGM",
+  "g": "GRM"
+};
+
+const CODE_TO_UNIT = {
+  "C62": "st",
+  "KGM": "kg",
+  "GRM": "g"
+};
 
 class OfferTypeResource extends Resource {
   @tracked value
@@ -29,34 +42,68 @@ class OfferTypeResource extends Resource {
   }
 }
 
-const UNIT_TO_CODE = {
-  "st": "C62",
-  "kg": "KGM",
-  "g": "GRM"
-};
+// Unpack offerings in unit, quantity, offering for easier processing
+async function unpackOfferings( offerings ) {
+  const rawOfferings = await offerings;
+  if (rawOfferings) {
+    let unpackedOfferings = [];
+    for ( const offer of rawOfferings ) {
+      const taq = await offer.typeAndQuantity;
+      unpackedOfferings.push( {
+        unit: taq.unit,
+        quantity: taq.unit == UNIT_TO_CODE["kg"] ? taq.value * 1000 : taq.value,
+        offering: offer
+      } );
+    }
+    return unpackedOfferings;
+  } else {
+    return [];
+  }
+}
+
+class UnpackedOfferingsResource extends Resource {
+  @tracked value;
+
+  async setup() {
+    const [offerings] = this.args.positional;
+    this.value = await unpackOfferings( offerings );
+  }
+}
 
 class AvailableOffersResource extends Resource {
   @tracked value
 
   async setup() {
-    const [offerings,unit] = this.args.positional;
+    const [unpackedOfferings,unit] = this.args.positional;
 
-    // console.log({offerings,unit});
+    // we want KGM and GR to be lumped together, otherwise just C62 (or whatever)
+    let offeringUnits = [];
+    if ( unit == "kg" || unit == "g" ) {
+      offeringUnits = [
+        UNIT_TO_CODE["kg"],
+        UNIT_TO_CODE["g"]
+      ];
+    } else {
+      offeringUnits = [ UNIT_TO_CODE[unit] ];
+    }
 
-    // const offerings = await product.sortedOfferings;
+    if( await unpackedOfferings ) {
+      // filter out the desired units
+      const consideredOfferingObjects = unpackedOfferings
+        .filter(({unit}) => offeringUnits.includes(unit));
 
-    const results = [];
+      // sort the offerings
+      const sortedOfferingObjects = consideredOfferingObjects
+        .sort(({quantity: a}, {quantity: b}) => a > b);
 
-    if( await offerings )
-      for (const offer of offerings) {
-        const taq = await offer.typeAndQuantity;
-        if (taq.unit == UNIT_TO_CODE[unit])
-          results.push(offer);
-      }
+      // extract the desired objects
+      const sortedOfferings = sortedOfferingObjects
+        .map( ({offering}) => offering );
 
-    // console.log({availableOfferings: results, unit });
-
-    this.value = results;
+      this.value = sortedOfferings;
+    } else {
+      this.value = [];
+    }
   }
 }
 
@@ -68,7 +115,6 @@ export default class ProductCardComponent extends Component {
   packageCount = 1
 
   @tracked selectedOffer = null;
-  @tracked selectedUnit = null;
 
   @service basket
 
@@ -80,47 +126,41 @@ export default class ProductCardComponent extends Component {
   }
 
   async asyncInit() {
+    this.selectedOffer = await this.defaultOffering();
+  }
+
+  async defaultOffering() {
+    // TODO: default offering should be a default offering chosen in the backend
     const offerings = (await this.args.product.offerings).toArray();
+    const unpacked = await unpackOfferings(offerings);
 
-    for( const offering of offerings ) {
-      await offering;
-      await offering.typeAndQuantity;
+    // first check the lowest amount of pieces which can be ordered,
+    if ( unpacked.find(({unit}) => unit == "C62") ) {
+      return unpacked
+        .filter(({unit}) => unit == "C62")
+        .sort(({quantity: a},{quantity: b}) => a > b)
+        .firstObject
+        .offering;
+    // then check the lowest amount of g or kgfor now
+    } else if ( unpacked.find(({unit}) => unit == "GRM" || unit == "KGM") ) {
+      return unpacked
+        .filter(({unit}) => unit == "GRM" || unit == "KGM")
+        .sort(({quantity: a},{quantity: b}) => a > b)
+        .firstObject
+        .offering;
+    // lastly just return the first object because this is weird
+    } else {
+      console.warn("Just returning the first available offering")
+      return unpacked.firstObject?.offering;
     }
-
-    this.selectedUnit = this.currentUnit;
-    // console.log({ units: this.units, offers: this.possibleOffers }); // init values
   }
 
   get currentUnit() {
-    if (this.selectedUnit) {
-      return this.selectedUnit;
-    } else {
-      return this.availableUnits?.firstObject;
-    }
+    return CODE_TO_UNIT[this.selectedOffer?.typeAndQuantity?.get("unit")];
   }
 
   get firstOffer() {
-    if (this.currentUnit) {
-      const unit = this.currentUnit;
-      return this
-        .args.product?.sortedOfferings?.filter( (offering) =>
-          offering?.typeAndQuantity?.get("unit") == UNIT_TO_CODE[unit]
-        ).firstObject;
-    } else {
-      return null;
-    }
-  }
-
-  get currentOffer(){
-    // It is not clear why we have to base ourselves on the id
-    // property in this case, but that seems to make it work.
-    if( this.selectedOffer?.id ){
-      return this.selectedOffer;
-    }
-    if( this.firstOffer?.id ){
-      return this.firstOffer;
-    }
-    return null;
+    return this.args.product?.sortedOfferings?.firstObject;
   }
 
   get detailClass() {
@@ -129,27 +169,40 @@ export default class ProductCardComponent extends Component {
 
   @action
   async add() {
-    this.basket.addOffer( this.currentOffer, this.packageCount );
-    await wait(500);
-    this.showDetail = false;
-    await wait(500);
-    this.selectedOffer = null;
-    this.packageCount = 1;
+    if( this.selectedOffer ) {
+      this.basket.addOffer( this.selectedOffer, this.packageCount );
+      await wait(500);
+      this.showDetail = false;
+      await wait(500);
+      this.selectedOffer = await this.defaultOffering();
+      this.packageCount = 1;
+    }
   }
 
   @action
-  selectValue(unit) {
-    this.selectedUnit = unit;
-    this.selectedOffer = null;
+  selectValue(selectedUnit) {
+    const firstFoundOffer = this
+      .unpackedOfferings?.find( ({unit}) => unit == UNIT_TO_CODE[selectedUnit] )?.offering;
+
+    this.selectedOffer = firstFoundOffer;
   }
 
-  @use
-  possibleOffers = new AvailableOffersResource( () => [this.args.product?.sortedOfferings, this.currentUnit] );
-
-  @use
-  units = new OfferTypeResource( () => [this.args.product] )
-
-  get availableUnits() {
-    return this.units;
+  @action
+  selectOffer(offer) {
+    if (offer instanceof OfferingModel) { // clicking to fast may yield an unitialized number component
+      this.selectedOffer = offer;
+    }
   }
+
+  // NOTE: we are sorting twice now, our current smarter sorting of
+  // possibleOffers should land in sortedOfferings and perhaps that can
+  // be made to supply the unpacked variant.
+  @use
+  unpackedOfferings = new UnpackedOfferingsResource( () => [this.args.product?.sortedOfferings] );
+
+  @use
+  possibleOffers = new AvailableOffersResource( () => [this.unpackedOfferings, this.currentUnit] );
+
+  @use
+  availableUnits = new OfferTypeResource( () => [this.args.product] )
 }
