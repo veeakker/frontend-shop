0df5a79c774bdd4dca5fcbd5c7f4d890e4f42e28
Aad Versteden <madnificent@gmail.com>
2024-11-17 17:52:28 +0100

    Add support for GlitchTip

---
+++ app/utils/glitchtip.js
@@ -0,0 +1,22 @@
+import { init as initGlitchtip } from '@sentry/ember';
+import config from 'veeakker/config/environment';
+
+export default function setupGlitchtip() {
+  try {
+    if (config.sentry.dsn !== '{{SENTRY_DSN}}') {
+      let sentryEnvironment =
+        config.sentry.environment !== '{{SENTRY_ENVIRONMENT}}'
+          ? config.sentry.environment
+          : 'production';
+
+      initGlitchtip({
+        dsn: config.sentry.dsn,
+        release: config.APP.version, // ember-cli-app-version sets this value
+        environment: sentryEnvironment,
+        autoSessionTracking: false, // Not implemented by GlitchTip so it triggers a lot of 501 "Not implemented" responses
+      });
+    }
+  } catch (e) {
+    console.warn(`Could not init glitchtip ${e}`);
+  }
+}
