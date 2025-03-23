48c7d9181c72a47eb1d45dbf5d2f7f00a207b291
Aad Versteden <madnificent@gmail.com>
2025-03-23 16:18:44 +0100

    Get lfwLink from delivery location if supplied
    When the lfwLink is specified on the location, we will redirect users in the
    finish flow to that specific URL.

--- app/models/delivery-place.js
+++ app/models/delivery-place.js
@@ -3,6 +3,7 @@ import Model, { belongsTo, attr } from '@ember-data/model';
 export default class DeliveryPlaceModel extends Model {
   @attr() label;
   @attr('boolean') isEnabled;
+  @attr() lfwLink;
   @belongsTo('delivery-kind') deliveryKind;
   @belongsTo('geo-coordinate') geoCoordinate;
   @belongsTo('postal-address') postalAddress;
