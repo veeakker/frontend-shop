8322f9a62f49cdd4feb2def539a5087f912f45ca
Aad Versteden <madnificent@gmail.com>
2025-06-22 17:35:56 +0200

    First support for /at/place to support local shops
    Currently sets the place of delivery of the current basket to the new location
    in the frontend.
    
    Future improvements:
    - specific support in the backend for merging baskets
    - detection of the deliveryKind based on the deliveryPlace

---
+++ app/routes/at-delivery-point.js
@@ -0,0 +1,21 @@
+import { service } from '@ember/service';
+import Route from '@ember/routing/route';
+
+export default class AtDeliveryPointRoute extends Route {
+  @service store;
+  @service basket;
+  @service router;
+
+  async model({slug}) {
+    const basket = await this.basket.pBasket;
+    const deliveryPlaces = await this.store.query('delivery-place', {
+      "filter[:exact:slug]": slug
+    });
+    if ( deliveryPlaces.length ) {
+      basket.deliveryPlace = deliveryPlaces[0];
+    }
+    // TODO get the deliveryType dynamically.
+    basket.deliveryType = "http://veeakker.be/delivery-methods/shop";
+    this.router.transitionTo("webshop");
+  }
+}
