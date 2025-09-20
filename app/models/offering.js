6fca3833cfe01715935b209205cc7fbc90b3c465
Aad Versteden <madnificent@gmail.com>
2025-09-20 12:31:13 +0200

    Extend offering model to indicate where it is available

--- app/models/offering.js
+++ app/models/offering.js
@@ -1,7 +1,8 @@
-import Model, { belongsTo } from '@ember-data/model';
+import Model, { belongsTo, hasMany } from '@ember-data/model';
 
 export default class OfferingModel extends Model {
   @belongsTo('unit-price-specification', { async: true, inverse: null }) unitPrice;
   @belongsTo('type-and-quantity', { async: true, inverse: null }) typeAndQuantity;
   @belongsTo('business-entity', { async: true, inverse: null }) supplier;
+  @hasMany('business-entity', { async: true, inverse: null }) availableAtOrFrom;
 }
