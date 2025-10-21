bec524498b8155f0370ad17d67db6c1648460b8e
Aad Versteden <madnificent@gmail.com>
2025-10-21 23:33:24 +0200

    Indicate non-availability of product on location in order-line

--- app/components/webshop/order-lines.js
+++ app/components/webshop/order-lines.js
@@ -4,6 +4,14 @@ import Component from '@glimmer/component';
 export default class WebshopOrderLinesComponent extends Component {
   @service basket;
 
+  /**
+    * Can be ordered on this location.
+   */
+  get canBeOrderedOnLocation() {
+
+    this.basket.constrainingBusinessEntity
+  }
+
   get persistCommentFn() {
     return (orderLine) => this.persistComment(orderLine);
   }
